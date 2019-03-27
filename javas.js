a73696ec4277eba6eb05dc00c5d198ba4a73d904
Ezio2220 <fjosue447@gmail.com>
2019-03-26 22:40:18 -0600

    Update javas.js

--- javas.js
+++ javas.js
@@ -14,15 +14,11 @@ function calcular(){
     bn=document.getElementById("bn").value;
     f=document.getElementById("fn").value;
     tol=parseFloat(Math.pow(10,-document.getElementById("tol").value));
-    criterio = "ER";//document.getElementById("crit").value;
+    criterio = document.getElementById("crit").value;
+    var acum=" ";
     do{
     ax=(parseFloat(an)+parseFloat(bn))/2;
-    f=f.replace("x^2","*x*x");
-    f=f.replace("x^3","*x*x*x");
-    f=f.replace("x^4","*x*x*x*x");
-    f=f.replace("x^5","*x*x*x*x*x");
-    f=f.replace("x","*"+ax);
-
+    f=f.replace("x",ax);
     fp=eval(f);
     if(n==1){
         Er=0;
@@ -30,17 +26,26 @@ function calcular(){
         Er=(parseFloat(ax)-parseFloat(Pn))/parseFloat(ax);
     }
     Pn=ax;
+    
     tab=document.getElementById("contenido");
-    tab.innerHTML= f+"<tr> <th scope='row'>"+n+"</th>"+
+    
+    acum+= "<tr> <th scope='row'>"+n+"</th>"+
     "<td>"+an+"</td><td>"+bn+"</td>"+
-    "<td>"+Pn+"</td><td>"+fp+"</td>"+
+    "<td>"+ax+"</td><td>"+fp+"</td>"+
     "<td>"+Er+"</td></tr>";
+    
+    
     if(fp>0){
-        bn=Pn;
+        bn=ax;
     }else{
-        an=Pn
+        an=ax;
     }
+     alert(n);
+    if(n==1){
+        Er=999;
+    }    
     n++;
     }while(n==3);
-
+    
+    tab.innerHTML=acum;
 }
